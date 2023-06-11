3aaf0c990e6006a5dd5a47b7162ba7febc5baeff
0xshoegazer <0xshoegazer@gmail.com>
2023-06-11 09:31:20 -0400

    reward data stuctures update

--- contracts/chef/PoolRewardManager.sol
+++ contracts/chef/PoolRewardManager.sol
@@ -12,7 +12,6 @@ contract PoolRewardManager is AccessControlUpgradeable {
     using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
 
     struct RewardToken {
-        IERC20MetadataUpgradeable token;
         uint256 sharesPerSecond;
         uint256 accTokenPerShare;
         uint256 PRECISION_FACTOR; // Account for varying decimals in calculations
@@ -20,22 +19,18 @@ contract PoolRewardManager is AccessControlUpgradeable {
 
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
-    uint256 public currentRewardCount;
-
     address public poolAddress;
     address public treasury;
 
     EnumerableSetUpgradeable.AddressSet private _rewardTokenAddresses;
-    RewardToken[] public rewardTokens;
-
     mapping(address => RewardToken) private _rewardTokens;
 
     mapping(uint256 => mapping(address => uint256)) public positionRewardDebts; // per token reward debt for each NFT position
 
-    event RewardTokenAdded(IERC20MetadataUpgradeable token, uint256 sharesPerSecond);
-    event RewardTokenUpdated(IERC20MetadataUpgradeable token, uint256 sharesPerSecond);
-    event RewardTokenRemoved(IERC20MetadataUpgradeable token);
-    event RewardTokenHarvested(IERC20MetadataUpgradeable token, uint256 amount);
+    event RewardTokenAdded(address token, uint256 sharesPerSecond);
+    event RewardTokenUpdated(address token, uint256 sharesPerSecond);
+    event RewardTokenRemoved(address token);
+    event RewardTokenHarvested(address token, uint256 amount);
 
     modifier onlyAdmin() {
         require(hasRole(ADMIN_ROLE, msg.sender), "Not an admin");
@@ -75,8 +70,24 @@ contract PoolRewardManager is AccessControlUpgradeable {
         return _rewardTokenAddresses.values();
     }
 
-    function getRewardTokens() external view returns (RewardToken[] memory) {
-        return rewardTokens;
+    function getRewardTokens() external view returns (RewardToken[] memory rewards) {
+        uint256 currentRewardTokenCount = getCurrentRewardCount();
+        rewards = new RewardToken[](currentRewardTokenCount);
+
+        address tokenAddress;
+
+        for (uint256 i = 0; i < currentRewardTokenCount; ) {
+            tokenAddress = _rewardTokenAddresses.at(i);
+            rewards[i] = _rewardTokens[tokenAddress];
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function getCurrentRewardCount() public view returns (uint256) {
+        return _rewardTokenAddresses.length();
     }
 
     /**
@@ -89,7 +100,7 @@ contract PoolRewardManager is AccessControlUpgradeable {
         uint256 lpSupplyWithMultiplier,
         uint256 lastRewardTime
     ) external view onlyPool returns (address[] memory tokens, uint256[] memory rewardAmounts) {
-        uint256 currentRewardTokenCount = rewardTokens.length;
+        uint256 currentRewardTokenCount = getCurrentRewardCount();
         tokens = new address[](currentRewardTokenCount);
         rewardAmounts = new uint256[](currentRewardTokenCount);
 
@@ -100,12 +111,13 @@ contract PoolRewardManager is AccessControlUpgradeable {
         uint256 rewardAmountForDuration;
         uint256 adjustedTokenPerShare;
         uint256 currentRewardDebt;
-
+        address tokenAddress;
         uint256 timeSinceLastReward = block.timestamp - lastRewardTime;
 
         for (uint256 i = 0; i < currentRewardTokenCount; ) {
-            currentReward = rewardTokens[i];
-            tokens[i] = address(currentReward.token);
+            tokenAddress = _rewardTokenAddresses.at(i);
+            currentReward = _rewardTokens[tokenAddress];
+            tokens[i] = tokenAddress;
 
             if (block.timestamp > lastRewardTime && lpSupplyWithMultiplier > 0) {
                 rewardAmountForDuration = timeSinceLastReward * currentReward.sharesPerSecond;
@@ -115,7 +127,7 @@ contract PoolRewardManager is AccessControlUpgradeable {
                     lpSupplyWithMultiplier;
                 adjustedTokenPerShare = currentReward.accTokenPerShare + adjustedTokenPerShare;
 
-                currentRewardDebt = positionRewardDebts[positionTokenId][address(currentReward.token)];
+                currentRewardDebt = positionRewardDebts[positionTokenId][tokenAddress];
                 rewardAmounts[i] =
                     (positionAmount * adjustedTokenPerShare) /
                     currentReward.PRECISION_FACTOR -
@@ -133,17 +145,20 @@ contract PoolRewardManager is AccessControlUpgradeable {
      * Only callable by pool
      */
     function updatePositionRewardDebts(uint256 positionAmountMultiplied, uint256 tokenId) external onlyPool {
-        if (rewardTokens.length == 0) {
+        uint256 currentRewardCount = getCurrentRewardCount();
+
+        if (currentRewardCount == 0) {
             return;
         }
 
         RewardToken memory currentReward;
-        uint256 rewardCount = rewardTokens.length;
+        address tokenAddress;
 
-        for (uint256 i = 0; i < rewardCount; ) {
-            currentReward = rewardTokens[i];
+        for (uint256 i = 0; i < currentRewardCount; ) {
+            tokenAddress = _rewardTokenAddresses.at(i);
+            currentReward = _rewardTokens[tokenAddress];
 
-            positionRewardDebts[tokenId][address(currentReward.token)] =
+            positionRewardDebts[tokenId][tokenAddress] =
                 (positionAmountMultiplied * currentReward.accTokenPerShare) /
                 currentReward.PRECISION_FACTOR;
 
@@ -158,18 +173,19 @@ contract PoolRewardManager is AccessControlUpgradeable {
      * Only callable by pool
      */
     function updateRewardsPerShare(uint256 lpSupplyMultiplied, uint256 lastRewardTime) external onlyPool {
-        uint256 rewardCount = rewardTokens.length;
+        uint256 currentRewardCount = getCurrentRewardCount();
 
-        if (rewardCount == 0 || lpSupplyMultiplied == 0) return;
+        if (currentRewardCount == 0 || lpSupplyMultiplied == 0) return;
 
-        uint256 currentDuration;
+        uint256 currentDuration = block.timestamp - lastRewardTime;
         uint256 rewardAmountForDuration;
         uint256 accTokenPerShare;
-        uint256 timeOfLastReward = lastRewardTime;
+        address tokenAddress;
 
-        for (uint256 i = 0; i < rewardCount; i++) {
-            RewardToken storage reward = rewardTokens[i];
-            currentDuration = block.timestamp - timeOfLastReward;
+        for (uint256 i = 0; i < currentRewardCount; i++) {
+            tokenAddress = _rewardTokenAddresses.at(i);
+            RewardToken storage reward = _rewardTokens[tokenAddress];
+
             rewardAmountForDuration = currentDuration * reward.sharesPerSecond;
             accTokenPerShare = (rewardAmountForDuration * reward.PRECISION_FACTOR) / lpSupplyMultiplied;
 
@@ -182,8 +198,9 @@ contract PoolRewardManager is AccessControlUpgradeable {
         address recipient,
         uint256 tokenId
     ) external onlyPool {
-        uint256 rewardCount = rewardTokens.length;
-        if (rewardCount == 0) {
+        uint256 currentRewardCount = getCurrentRewardCount();
+
+        if (currentRewardCount == 0) {
             return;
         }
 
@@ -191,20 +208,20 @@ contract PoolRewardManager is AccessControlUpgradeable {
 
         RewardToken memory currentReward;
         uint256 pendingAmount;
-        address rewardAddress;
+        address tokenAddress;
 
-        for (uint256 i = 0; i < rewardCount; ) {
-            currentReward = rewardTokens[i];
-            rewardAddress = address(currentReward.token);
+        for (uint256 i = 0; i < currentRewardCount; ) {
+            tokenAddress = _rewardTokenAddresses.at(i);
+            currentReward = _rewardTokens[tokenAddress];
 
             pendingAmount =
                 (positionAmountMultiplied * currentReward.accTokenPerShare) /
                 currentReward.PRECISION_FACTOR -
-                positionRewardDebts[tokenId][rewardAddress];
+                positionRewardDebts[tokenId][tokenAddress];
 
             if (pendingAmount > 0) {
-                emit RewardTokenHarvested(currentReward.token, pendingAmount);
-                _safeRewardsTransfer(rewardAddress, recipient, pendingAmount);
+                emit RewardTokenHarvested(tokenAddress, pendingAmount);
+                _safeRewardsTransfer(tokenAddress, recipient, pendingAmount);
             }
 
             unchecked {
@@ -225,70 +242,42 @@ contract PoolRewardManager is AccessControlUpgradeable {
         uint256 decimalsRewardToken = uint256(token.decimals());
         require(decimalsRewardToken < 30, "Must be less than 30");
 
-        // rewardTokens.push(
-        //     RewardToken({
-        //         token: token,
-        //         sharesPerSecond: sharesPerSecond,
-        //         accTokenPerShare: 0,
-        //         PRECISION_FACTOR: uint256(10 ** (uint256(30) - decimalsRewardToken))
-        //     })
-        // );
-
-        currentRewardCount++;
-
         _rewardTokenAddresses.add(tokenAddress);
         _rewardTokens[tokenAddress] = RewardToken({
-            token: token,
             sharesPerSecond: sharesPerSecond,
             accTokenPerShare: 0,
             PRECISION_FACTOR: uint256(10 ** (uint256(30) - decimalsRewardToken))
         });
 
-        emit RewardTokenAdded(token, sharesPerSecond);
+        emit RewardTokenAdded(tokenAddress, sharesPerSecond);
     }
 
-    function updateRewardToken(address tokenAddress, uint256 tokenIndex, uint256 sharesPerSecond) external onlyAdmin {
-        _validateToken(tokenAddress, tokenIndex);
-
-        RewardToken storage reward = rewardTokens[tokenIndex];
-        require(address(reward.token) == tokenAddress, "Wrong token address for token index");
+    function updateRewardToken(address tokenAddress, uint256 sharesPerSecond) external onlyAdmin {
+        _validateToken(tokenAddress);
 
         _rewardTokens[tokenAddress].sharesPerSecond = sharesPerSecond;
 
-        reward.sharesPerSecond = sharesPerSecond;
-        emit RewardTokenUpdated(reward.token, sharesPerSecond);
+        emit RewardTokenUpdated(tokenAddress, sharesPerSecond);
     }
 
-    function removeRewardToken(address tokenAddress, uint256 tokenIndex) external {
+    function removeRewardToken(address tokenAddress) external {
         require(msg.sender == treasury, "Only treasury");
-        _validateToken(tokenAddress, tokenIndex);
+        _validateToken(tokenAddress);
 
-        // RewardToken memory reward = rewardTokens[tokenIndex];
-        // _rewardTokenAddresses.remove(address(reward.token));
-
-        // uint256 balance = reward.token.balanceOf(address(this));
-        // if (balance > 0) {
-        //     reward.token.safeTransfer(treasury, balance);
-        // }
-
-        // delete rewardTokens[tokenIndex];
-
-        RewardToken memory reward = rewardTokens[tokenIndex];
-        _rewardTokenAddresses.remove(address(reward.token));
-
-        uint256 balance = reward.token.balanceOf(address(this));
-        if (balance > 0) {
-            reward.token.safeTransfer(treasury, balance);
-        }
-
+        _rewardTokenAddresses.remove(tokenAddress);
         delete _rewardTokens[tokenAddress];
 
-        emit RewardTokenRemoved(reward.token);
+        IERC20MetadataUpgradeable rewardToken = IERC20MetadataUpgradeable(tokenAddress);
+        uint256 balance = rewardToken.balanceOf(address(this));
+        if (balance > 0) {
+            rewardToken.safeTransfer(treasury, balance);
+        }
+
+        emit RewardTokenRemoved(tokenAddress);
     }
 
-    function _validateToken(address tokenAddress, uint256 tokenIndex) internal view {
+    function _validateToken(address tokenAddress) internal view {
         require(tokenAddress != address(0), "Token not provided");
-        // require(tokenIndex < rewardTokens.length, "Invalid token index");
         require(_rewardTokenAddresses.contains(tokenAddress), "Token not added");
     }
 
