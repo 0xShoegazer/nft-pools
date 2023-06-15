4fcf7962c7a6a0cb613d47d4b57e325561f0d9d8
0xshoegazer <0xshoegazer@gmail.com>
2023-06-15 08:20:17 -0400

    specs

--- test/global-manager.spec.ts
+++ test/global-manager.spec.ts
@@ -4,19 +4,29 @@ import { expect } from 'chai';
 import { createPool } from '../scripts/utils';
 
 describe('RewardManager', () => {
-  describe('adding pools', async () => {
+  describe('Adding pools', async () => {
     it('factory adds pools at creation time', async () => {
       const { globalRewardManager, lpPoolAddress, factory, signer } = await loadFixture(managerFixture);
       // await globalRewardManager.addPool(nftPoolAddress);
 
       // factory will add pool to global reward manager
+      await createPool(factory.address, lpPoolAddress, globalRewardManager.address, signer);
+
+      expect(await globalRewardManager.getPoolCount()).to.equal(1);
+    });
+
+    it('allows admins to add a pool', async () => {
+      const { globalRewardManager, lpPoolAddress, factory, signer } = await loadFixture(managerFixture);
+
+      // contracts are set with admin signer at this point
       const nftPoolAddress: string = await createPool(
         factory.address,
         lpPoolAddress,
         globalRewardManager.address,
         signer
       );
-      console.log('nftPoolAddress: ' + nftPoolAddress);
+
+      await globalRewardManager.addPool(nftPoolAddress);
 
       expect(await globalRewardManager.getPoolCount()).to.equal(1);
     });
