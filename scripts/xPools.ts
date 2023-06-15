c961c5cbd002ac0c638340ac535a09149693a95c
jumpman23 <102991359+jumpman23xx@users.noreply.github.com>
2023-06-15 11:28:40 -0400

    addys

--- scripts/xPools.ts
+++ scripts/xPools.ts
@@ -9,4 +9,124 @@ export const xPools = {
     lpPoolAddress: '0xcf8ea96ecfdb7800e1eb413bde4af6ce984ab079',
     initialChefAllocationPoints: 10,
   },
+//   {
+//     pid: 7,
+//     lpSymbol: 'USD+-DAI+ LP',
+//     lpAddresses: {
+//       42161: '0xE8C060d40D7Bc96fCd5b758Bd1437C8653400b0e',
+//     },
+//    
+//   {
+//     pid: 8,
+//     lpSymbol: 'USD+-USDC LP',
+//     lpAddresses: {
+//       42161: '0xECe52B1fc32D2B4f22eb45238210b470a64bfDd5',
+//   },
+
+//   {
+//     pid: 15,
+//     lpSymbol: 'ARX-ETH LP',
+//     lpAddresses: {
+//       42161: '0x62FdDfC2D4b35aDec79c6082CA2894eAb01aC0db',
+//     },
+
+//   {
+//     pid: 16,
+//     lpSymbol: 'DAI-DAI+ LP',
+//     lpAddresses: {
+//       42161: '0xeE5e74Dc56594d070E0827ec270F974A68EBAF22',
+//     },
+//   {
+//     pid: 17,
+//     lpSymbol: 'USD+-ETH LP',
+//     lpAddresses: {
+//       42161: '0xEa5f97aab76E397E4089137345c38b5c4e7939B3',
+//   },
+//   {
+//     pid: 21,
+//     v1pid: 0,
+//     lpSymbol: 'WBTC-ETH LP',
+//     lpAddresses: {
+//       97: '0x8a8d968c9534727118aa02965f16f6d398c14b1d',
+//       56: '0x8a8d968c9534727118aa02965f16f6d398c14b1d',
+//       42161: '0x8a8d968c9534727118aa02965f16f6d398c14b1d',
+//     },
+//     token: serializedTokens.wbtc,
+//     quoteToken: serializedTokens.wbnb,
+//     quantum: true,
+//     wide: true,
+//   },
+//   {
+//     pid: 22,
+//     lpSymbol: 'WETH-USDC LP',
+//     lpAddresses: {
+//       42161: '0xcf8ea96ecfdb7800e1eb413bde4af6ce984ab079',
+//     },
+//     nftPoolAddress: {
+//       42161: '0xB98ced1fab671Ac55562F28d0E40af0BDDeE4065',
+//     },
+//   },
+//   {
+//     pid: 23,
+//     lpSymbol: 'USDT-USDC LP',
+//     lpAddresses: {
+//       42161: '0x3fb86a978530d9881627907b428e9ac9c02f9406',
+//   },
+//   {
+//     pid: 24,
+//     lpSymbol: 'DAI-USDC LP',
+//     lpAddresses: {
+//       42161: '0xc4602d1415f32ae23a72ad8cf4371272e25e2f22',
+//   },
+//   {
+//     pid: 25,
+//     v1pid: 0,
+//     lpSymbol: 'WBTC-USDC LP',
+//     lpAddresses: {
+//       42161: '0x02ebfb3aac7934cdc6877d5ee8cf5e17e449a9a3',
+//     },
+//   {
+//     pid: 26,
+//     lpSymbol: 'ARB-USDC LP',
+//     lpAddresses: {
+//       42161: '0xc1e2eda42ae3b3da68b622f03cd04ff3412467f7',
+//     },
+//   {
+//     pid: 27,
+//     lpSymbol: 'ARB-ETH LP',
+//     lpAddresses: {
+//       42161: '0x3369839e15d31b884dbd4e8e101fda0125fb08e8',
+//     },
+//   },
+//   {
+//     pid: 28,
+//     lpSymbol: 'RDNT-WETH LP',
+//     lpAddresses: {
+//       42161: '0xaca4edeee3a98a4311ec4c2a85c92163881f1644',
+//     },
+//   },
+//   {
+//     pid: 29,
+//     lpSymbol: 'FRAX-USDC LP',
+//     lpAddresses: {
+//       42161: '0x2d3d9D377C046C218e0F3B5A6bD17C9E673F1d8C',
+//     },
+//   },
+//   {
+//     pid: 30,
+//     lpSymbol: 'WETH-USDC LP',
+//     lpAddresses: {
+//       42161: '0xa1c443f7ae3cec5a61a163b98461af6dc4a98a90',
+//     },
+//   },
+//   {
+//     pid: 31,
+//     lpSymbol: 'GNS-WETH LP',
+//     lpAddresses: {
+//     },
+//   },
+// ]
 };
+
+
+
