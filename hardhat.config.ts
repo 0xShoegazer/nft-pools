4e8a4ba6ee7513660ac440036a820c5b147adadd
0xBriz <chaindevone@gmail.com>
2023-07-14 16:47:31 -0400

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,71 @@
+import * as dotenv from 'dotenv';
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-etherscan';
+import 'hardhat-contract-sizer';
+
+dotenv.config();
+
+const accounts = process.env.DEV_KEY !== undefined ? [process.env.DEV_KEY] : [];
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.7.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  contractSizer: {
+    // override defaults as needed: https://www.npmjs.com/package/hardhat-contract-sizer
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+    customChains: [
+      {
+        network: 'base',
+        chainId: 8453,
+        urls: {
+          apiURL: 'https://api.basescan.org',
+          browserURL: 'https://basescan.org',
+        },
+      },
+    ],
+  },
+  gasReporter: {
+    enabled: true,
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ARBITRUM_RPC || '',
+        blockNumber: 97443294,
+      },
+    },
+    arbitrum: {
+      url: process.env.ARBITRUM_RPC || '',
+      accounts,
+    },
+    optimism: {
+      url: `${process.env.OPTIMISM_RPC}`,
+      accounts,
+    },
+    base: {
+      url: `https://developer-access-mainnet.base.org/`,
+      accounts,
+      gasPrice: 2500000000,
+    },
+    bsc: {
+      url: process.env.BSC_RPC || '',
+      accounts,
+    },
+  },
+};
+
+export default config;
