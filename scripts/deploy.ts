f59f10483ecc1c83845d2c199a027bf48b48eb4b
0xshoegazer <0xshoegazer@gmail.com>
2023-08-18 09:59:03 -0400

    nft pools

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -24,9 +24,34 @@ interface PoolInfo {
   lpAddress: string;
   allocationPoints: number;
   allocationPointsWETH: number;
-  created: boolean;
-  addedToChef: boolean;
 }
+
+//   {
+//     pid: 10,
+//     lpSymbol: 'USD+-USDC LP',
+//     lpAddresses: {
+//       [ChainId.BASE]: '0x696b4d181Eb58cD4B54a59d2Ce834184Cf7Ac31A',
+//     },
+//     token: serializedTokens.usdp,
+//     quoteToken: serializedTokens.usdbc,
+//     classic: true,
+//   },
+
+// 1 - 0xE80B4F755417FB4baF4dbd23C029db3F62786523 - 2045
+// 2 - 0x81a03d61c913BdcC60519423C8841C18FfB752a8 - 5
+// 3 - 0x9A0b05F3cF748A114A4f8351802b3BFfE07100D4 - 200
+// 5 - 0x6D3c5a4a7aC4B1428368310E4EC3bB1350d01455 - 205
+// 6 - 0x07CFA5Df24fB17486AF0CBf6C910F24253a674D3 - 1300
+// 7 - 0x41d160033C222E6f3722EC97379867324567d883 - 230
+// 8 - 0xa2b120Cab75AEfDfAFdA6A14713349A3096EED79 - 30
+// 9 - 0x2135780D04C96E14bC205d2c8B8eD4e716d09A2b - 25
+// 10 - 0x696b4d181Eb58cD4B54a59d2Ce834184Cf7Ac31A - 50
+// 11 - 0x7Fb35b3967798cE8322cC50eF52553BC5Ee4c306 - 50
+// 12 - 0x6EDa0a4e05fF50594E53dBf179793CADD03689e5 - 50
+// 13 - 0x1cd6Ca847016A3bd0cC1fe2dF5027E78ea428170 - 50
+// 14 - 0x317d373E590795e2c09D73FaD7498FC98c0A692B - 25
+// 15 - 0x30dcc8444F8361D5CE119fC25e16AF0B583e88Fd - 50
+// 16 - 0x29399d824a99789f587a491C59210326e8ef4545 - 1
 
 // @note this is using arbidex dev account instead of baseswap deployer
 // TODO: transfer ownerships as needed after setup
@@ -47,7 +72,7 @@ async function main() {
     // const bsx = await ethers.getContractAt('BaseXToken', PROTOCOL_TOKEN, signer);
     // await bsx.initializeMasterAddress(CHEF);
 
-    // TODO: Set start time once presale and liquidity things are settled
+    // TODO: INIT START TIME: Set start time once presale and liquidity things are settled
     const emissionRate = parseUnits('0.15');
     // const blockTime = await getCurrentBlockTime(ethers.provider)
     // const start = blockTime + 120
@@ -63,20 +88,18 @@ async function main() {
     // await deployPoolFactory(CHEF, PROTOCOL_TOKEN, XTOKEN, signer);
 
     // const factory = await ethers.getContractAt('NFTPoolFactory', FACTORY, signer);
+    const factory = await ethers.getContractAt('NFTPoolFactory', FACTORY, signer);
+    console.log(await factory.getPool('0x696b4d181Eb58cD4B54a59d2Ce834184Cf7Ac31A'));
 
-    // TODO: Create the NFT pools and add to chef for current PCS farms/pools
-    const pools: PoolInfo[] = [
-      {
-        pool: 'BBT-ETH',
-        lpAddress: '0xf4b96d5162adee867b6361e9f1848d701c4286c7',
-        allocationPoints: 1,
-        allocationPointsWETH: 1,
-        created: true,
-        addedToChef: true,
-      },
-    ];
-
-    //  await createPools(pools, signer);
+    // await createNFTPool(
+    //   {
+    //     pool: 'USD+-USDC',
+    //     lpAddress: '0x696b4d181Eb58cD4B54a59d2Ce834184Cf7Ac31A',
+    //     allocationPoints: 50,
+    //     allocationPointsWETH: 50,
+    //   },
+    //   signer
+    // );
 
     // TODO: updatePool on any pools created then before "go live"
   } catch (error) {
@@ -85,21 +108,7 @@ async function main() {
   }
 }
 
-async function createPools(pools: PoolInfo[], signer: SignerWithAddress) {
-  pools = pools.filter((p) => !p.created);
-
-  for (const pool of pools) {
-    const nftPoolAddress = await createNFTPool(pool, signer);
-    await sleepWait();
-    await addPoolToChef(nftPoolAddress, pool, true, signer);
-  }
-}
-
 async function createNFTPool(pool: PoolInfo, signer: SignerWithAddress) {
-  if (pool.addedToChef) {
-    console.log('Pool already added to chef');
-    return;
-  }
   console.log(`Creating NFTPool for pool: ${pool.pool}`);
 
   const factory = await ethers.getContractAt('NFTPoolFactory', FACTORY, signer);
@@ -109,7 +118,11 @@ async function createNFTPool(pool: PoolInfo, signer: SignerWithAddress) {
   const nftPoolAddress = ethers.utils.defaultAbiCoder.decode(['address'], rx.events[1].data);
   console.log('NFTPool created: ' + nftPoolAddress[0]);
 
-  return nftPoolAddress[0];
+  nftPoolAddress[0];
+
+  await sleepWait();
+
+  await addPoolToChef(nftPoolAddress[0], pool, false, signer);
 }
 
 async function addPoolToChef(nftPoolAddress: string, pool: PoolInfo, withUpdate: boolean, signer: SignerWithAddress) {
